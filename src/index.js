25afb6e302315c9fb53b9019f86bc2080270200e
jpreming <jpreming@gmail.com>
2020-03-12 22:12:05 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router} from 'react-router-dom';
+import LoginComponent from './login/login';
+import SignupComponent from './signup/signup';
+import DashboardComponent from './dashboard/dashboard';
+
+const firebase= require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({ 
+apiKey: "AIzaSyB8Sj7rq-a0nZSYgjHLwW9GmIqe_6mNv7o",
+authDomain: "im-tutorial-a3aaa.firebaseapp.com",
+databaseURL: "https://im-tutorial-a3aaa.firebaseio.com",
+projectId: "im-tutorial-a3aaa",
+storageBucket: "im-tutorial-a3aaa.appspot.com",
+messagingSenderId: "775229379819",
+appId: "1:775229379819:web:4e90ef4b2a3b5c9e935aae",
+measurementId: "G-TX9Z0LXYLX"
+});
+
+const routing= (
+    <Router>
+        <div id='routing-container'>
+            <Route path='/login' component={LoginComponent}></Route>
+            <Route path='/signup' component={SignupComponent}></Route>
+            <Route path='/dashboard' component={DashboardComponent}></Route>
+
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
